c5556b0ff03358631b5c5ddb4fa7c5a8b6c40ca9
George N. - Macbook <91430969+Gevoooo@users.noreply.github.com>
2025-07-04 11:10:29 -0400

    feat: start setting up db queries

---
+++ dbQueries.js
@@ -0,0 +1,32 @@
+// TODO
+export async function getTotalOwed(discordId) {
+	const db = getDB();
+	const collection = db.collection("debts");
+
+	// Your database operations
+	const result = await collection.find({}).toArray();
+	// Handle the result
+	return result;
+}
+
+// TODO
+export async function getAllDebtors(discordId) {
+	const db = getDB();
+	const collection = db.collection("debts");
+
+	// Your database operations
+	const result = await collection.find({}).toArray();
+	// Handle the result
+	return result;
+}
+
+// TODO
+export async function getAllCreditors(discordId) {
+	const db = getDB();
+	const collection = db.collection("debts");
+
+	// Your database operations
+	const result = await collection.find({}).toArray();
+	// Handle the result
+	return result;
+}
