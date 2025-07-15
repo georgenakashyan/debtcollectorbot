a201ec42a20ee8ac4cf4a8c8e6688e8ea24a4ac8
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-07-14 23:51:26 -0400

    feat: add query to get all transactions between users

--- src/db/dbQueries.js
+++ src/db/dbQueries.js
@@ -148,80 +148,10 @@ export async function getUserCredits(guildId, userId) {
 	return results[0] || { totalAmount: 0, debtCount: 0 };
 }
 
-// 5. Get debt summary for a user (both debts and credits)
-// TODO
-export async function getUserDebtSummary(guildId, userId) {
+export async function getAllTransactionsFromSomeone(creditorId, debtorId) {
 	const db = getDB();
-
-	const summary = await db.debts.aggregate([
-		{
-			$match: {
-				$or: [{ debtorId: userId }, { creditorId: userId }],
-				guildId: guildId,
-				isSettled: false,
-			},
-		},
-		{
-			$group: {
-				_id: null,
-				totalOwed: {
-					$sum: {
-						$cond: [{ $eq: ["$debtorId", userId] }, "$amount", 0],
-					},
-				},
-				totalOwedToMe: {
-					$sum: {
-						$cond: [{ $eq: ["$creditorId", userId] }, "$amount", 0],
-					},
-				},
-			},
-		},
-	]);
-	return summary[0] || { totalOwed: 0, totalOwedToMe: 0 };
-}
-
-// 6. Settle a debt (mark as paid)
-// TODO
-export async function settleDebt(guildId, debtId, settlementMethod = null) {
-	const db = getDB();
-
-	return await db.debts.updateOne(
-		{ _id: debtId },
-		{
-			$set: {
-				guildId: guildId,
-				isSettled: true,
-				settledAt: new Date(),
-				"metadata.settlementMethod": settlementMethod,
-			},
-		}
-	);
-}
-
-// 7. Get guild debt leaderboard
-// TODO
-export async function getGuildDebtLeaderboard(guildId, limit = 10) {
-	const db = getDB();
-
-	return await db.debts.aggregate([
-		{
-			$match: {
-				guildId: guildId,
-				isSettled: false,
-			},
-		},
-		{
-			$group: {
-				_id: "$debtorId",
-				totalDebt: { $sum: "$amount" },
-				debtCount: { $sum: 1 },
-			},
-		},
-		{
-			$sort: { totalDebt: -1 },
-		},
-		{
-			$limit: limit,
-		},
-	]);
+	const results = await db.debts
+		.find({ creditorId: creditorId, debtorId: debtorId, isSettled: false })
+		.toArray();
+	return results || [];
 }
