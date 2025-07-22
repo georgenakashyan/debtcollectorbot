cea95519c7bf71cc98f8b5d00aa9cc7a48d83161
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-07-21 23:01:38 -0400

    feat: add owed command

---
+++ src/commands/utility/owed.js
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from "discord.js";
+import { getUserCredits } from "../../db/dbQueries.js";
+import { pluralize } from "../../utils/utils.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("owed")
+		.setDescription("The total you are owed by others in this server"),
+	async execute(interaction) {
+		const userId = interaction.user.id;
+		const guildId = interaction.guild.id;
+		const credit = await getUserCredits(guildId, userId);
+		// interaction.guild is the object representing the Guild in which the command was run
+		await interaction.reply(
+			`<@${userId}> is owed $${credit.totalAmount} in this server ${
+				credit.debtCount > 0
+					? pluralize(
+							`from ${credit.debtCount} transaction`,
+							credit.debtCount
+					  )
+					: ""
+			}`
+		);
+	},
+};
