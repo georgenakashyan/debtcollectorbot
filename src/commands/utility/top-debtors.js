2c6ca971e32763fe96a68ea04fedaab28de6449b
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-07-21 23:39:45 -0400

    feat: add top-debtors command

---
+++ src/commands/utility/top-debtors.js
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder } from "discord.js";
+import { getTopDebtors } from "../../db/dbQueries.js";
+import { buildTopDebtorLeaderboard } from "../../utils/utils.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("top-debtors")
+		.setDescription("Leaderboard of top debtors in this server"),
+	async execute(interaction) {
+		const guildId = interaction.guild.id;
+		const limit = 10;
+		const debtors = await getTopDebtors(guildId, limit);
+		const leaderboardContent = buildTopDebtorLeaderboard(debtors, limit);
+
+		await interaction.reply(leaderboardContent);
+	},
+};
