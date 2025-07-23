514ed25ba8f8a09fd1593f8fc8e930187888131e
George N. - Macbook <91430969+Gevoooo@users.noreply.github.com>
2025-07-22 20:19:49 -0400

    bug: fix import

--- src/utils/buildTopDebtorLeaderboard.js
+++ src/utils/buildTopDebtorLeaderboard.js
@@ -1,5 +1,20 @@
 import { formatNumber, pluralize } from "./utils.js";
 
+export function getOrdinalSuffix(i) {
+	var j = i % 10,
+		k = i % 100;
+	if (j == 1 && k != 11) {
+		return i + "st";
+	}
+	if (j == 2 && k != 12) {
+		return i + "nd";
+	}
+	if (j == 3 && k != 13) {
+		return i + "rd";
+	}
+	return i + "th";
+}
+
 export function leaderboardEmoji(placement) {
 	if (placement > 3) return "";
 	return ["🥇", "🥈", "🥉"].at(placement - 1);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,23 +2,6 @@ export function pluralize(word, count) {
 	return count === 1 ? word : word + "s";
 }
 
-export function getOrdinalSuffix(i) {
-	var j = i % 10,
-		k = i % 100;
-	if (j == 1 && k != 11) {
-		return i + "st";
-	}
-	if (j == 2 && k != 12) {
-		return i + "nd";
-	}
-	if (j == 3 && k != 13) {
-		return i + "rd";
-	}
-	return i + "th";
-}
-
-
-
 export function formatNumber(num) {
 	return Math.abs(
 		Math.min(Math.max(Math.round(num * 100) / 100, -100000), 100000)
