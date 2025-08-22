60943196fb1bf38c88a9ebb059c00d9df0f884b3
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-08-22 18:48:41 -0400

    bug: add missing ephemeral on all debts paid message

--- src/commands/utility/transactions.js
+++ src/commands/utility/transactions.js
@@ -500,6 +500,7 @@ export default {
 					if (allTransactions.length === 0) {
 						await componentInt.reply({
 							content: `✅ Transaction settled! <@${debtorId}> has paid off all their debts to <@${userId}>. `,
+							flags: MessageFlags.Ephemeral,
 						});
 						collector.stop();
 						return;
